ae7909cbc9179337e7dbaf472cf66f93fd9c4c2f
santhan29 <santhanpeddaguravagari@gmail.com>
2024-09-30 11:09:13 +0530

    terraform

---
+++ 40-apps/expense.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+dnf install ansible -y 
+cd /tmp 
+git clone https://github.com/santhan29/expense-ansible.git
+cd expense-ansible
+ansible-playbook -i inventory.ini mysql.yaml 
+ansible-playbook -i inventory.ini backend.yaml 
+ansible-playbook -i inventory.ini frontend.yaml  
